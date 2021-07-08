b2b4490f9283c977a417697ebba80d8d59406488
jinyeong-afk <xoeh7002@naver.com>
2021-07-09 03:03:43 +0900

    회원가입 및 관리 백엔드 개발 및 테스트

---
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,22 @@
+package hello.hellospring.domain;
+
+public class Member {
+    private Long id;
+    private String name;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/domain/MemberController.java
@@ -0,0 +1,8 @@
+package hello.hellospring.domain;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+}
