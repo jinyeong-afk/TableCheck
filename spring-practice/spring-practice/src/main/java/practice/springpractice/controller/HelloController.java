dd10731507e4f011fada28921c2f9f83d5914464
jinyeong-afk <xoeh7002@naver.com>
2021-08-10 03:28:47 +0900

    MVC 구조 Api 이해

--- spring-practice/spring-practice/src/main/java/practice/springpractice/controller/HelloController.java
+++ spring-practice/spring-practice/src/main/java/practice/springpractice/controller/HelloController.java
@@ -3,6 +3,8 @@ package practice.springpractice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,38 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "hello!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api") // json 방식
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+
+        private String name;
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
     }
 }
