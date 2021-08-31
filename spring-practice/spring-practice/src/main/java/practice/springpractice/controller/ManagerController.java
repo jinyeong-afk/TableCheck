793fbeae19ef1f56cb1203ef61134a8444256290
jinyeong-afk <xoeh7002@naver.com>
2021-09-01 03:44:08 +0900

    버튼 클릭시 좌석 현황 변경

--- spring-practice/spring-practice/src/main/java/practice/springpractice/controller/ManagerController.java
+++ spring-practice/spring-practice/src/main/java/practice/springpractice/controller/ManagerController.java
@@ -8,6 +8,8 @@ import practice.springpractice.domain.Store;
 import practice.springpractice.service.MemberService;
 import practice.springpractice.service.StoreService;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -30,7 +32,11 @@ public class ManagerController {
     @PostMapping("store/manage")
     public String postStoreManage(StoreForm storeForm, Model model) {
         Store store_table = new Store();
-        store_table.setTable_status(storeForm.getTable_status());
+        char checking = overCheck(storeForm.getTable_status());
+        if(checking !='0') {
+            store_table.setTable_status(storeForm.getTable_status().replace(Character.toString(checking), ""));
+        }
+        else store_table.setTable_status(storeForm.getTable_status());
         store_table.setId(storeForm.getId());
         if(storeForm.getTable_status() != null)
         {
@@ -41,6 +47,37 @@ public class ManagerController {
         return "Manager/storeManage";
     }
 
+    public char overCheck(String value) {
+        if(value != null)
+        {
+            char[] StoChar = value.toCharArray();
+            Arrays.sort(StoChar);
+            for(int i=0; i<StoChar.length-1; i++)
+            {
+                if(StoChar[i] == StoChar[i+1]) {
+                    return StoChar[i];
+                }
+            }
+        }
 
+        return '0';
+    }
+
+//    public String overRemove(String value) {
+//        char[] StoChar = value.toCharArray();
+//        Arrays.sort(StoChar);
+//        List<Character> values = new ArrayList<>();
+//        for(int i=0; i<StoChar.length; i++)
+//        {
+//            if(StoChar[i] != StoChar[i+1]) {
+//                values.add(StoChar[i]);
+//            }
+//        }
+//        String result = "";
+//        for(char val : values) {
+//            result += val;
+//        }
+//        return result;
+//    }
 
 }
