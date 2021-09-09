57dc96e081bcb609f87f391219c7ce2b314eaecd
jinyeong-afk <xoeh7002@naver.com>
2021-09-09 23:43:17 +0900

    time select option 구현

--- spring-practice/spring-practice/src/main/java/practice/springpractice/controller/GuestController.java
+++ spring-practice/spring-practice/src/main/java/practice/springpractice/controller/GuestController.java
@@ -3,6 +3,7 @@ package practice.springpractice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import practice.springpractice.domain.Store;
 import practice.springpractice.service.MemberService;
@@ -24,7 +25,7 @@ public class GuestController {
         this.storeService = storeService;
     }
     @PostMapping("Guest/reserve")
-    public String tableReserve(StoreForm storeForm, HttpServletResponse response) throws IOException {
+    public String tableReserve(StoreForm storeForm, Model model, HttpServletResponse response) throws IOException {
         Store store = new Store();
         store.setTable_status(storeForm.getTable_status());
         store.setId(storeForm.getId());
@@ -34,14 +35,25 @@ public class GuestController {
             return "redirect:/";
         }
         else {
+            model.addAttribute("num", 1);
             return "Guest/reservation";
         }
+    }
+
+    @PostMapping("Guest/tableCheck2")
+    public String gettableCheck(StoreForm storeForm, Model model, HttpServletResponse response) throws IOException {
+        Store store = storeService.tableCheck(storeForm.getStore_name());
+        model.addAttribute("store", store);
+        System.out.println("다시 왔단다2");
+        ScriptUtils.alertAndBackPage(response, "뒤로 가자");
+        return "Guest/main";
     }
 
     @PostMapping("Guest/tableCheck")
     public String tableCheck(StoreForm storeForm, Model model) {
         Store store = storeService.tableCheck(storeForm.getStore_name());
         model.addAttribute("store", store);
+        System.out.println("다시 왔단다");
         return "Guest/table";
     }
 
---
+++ spring-practice/spring-practice/src/main/java/practice/springpractice/domain/Reservation.java
@@ -0,0 +1,4 @@
+package practice.springpractice.domain;
+
+public class Reservation {
+}
